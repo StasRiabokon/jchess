cff2229ecfcefb4fb1960faa9c8202d8f34c8d5b
StasRyabokon97 <ya.stas.rybokon@gmail.com>
2016-03-25 19:31:41 +0200

    I do not know something to write

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -5,7 +5,7 @@ import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
 
-import java.util.List;
+import java.util.Collection;
 
 public abstract class Piece {
 
@@ -20,7 +20,7 @@ public abstract class Piece {
     public Alliance getPieceAlliance(){
         return this.pieceAlliance;
     }
-    public abstract List<Move> calculateLegalMoves(final Board board);
+    public abstract Collection<Move> calculateLegalMoves(final Board board);
 
 
 }
