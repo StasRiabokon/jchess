c115558a9acb9f62808942394d30c9c965cb37a7
StasRyabokon97 <ya.stas.rybokon@gmail.com>
2016-04-18 13:58:57 +0300

    The Pawn part 2

--- src/com/chess/engine/pieces/Pawn.java
+++ src/com/chess/engine/pieces/Pawn.java
@@ -6,6 +6,8 @@ import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
 import com.chess.engine.board.Move.MajorMove;
 import com.chess.engine.board.Tile;
+import com.google.common.collect.ImmutableList;
+import jdk.nashorn.internal.ir.annotations.Immutable;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -16,7 +18,7 @@ import java.util.List;
  */
 public class Pawn extends Piece {
 
-    private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = {8};
+    private final static int[] CANDIDATE_MOVE_COORDINATE = {8,16};
 
     public Pawn(final int piecePosition, final Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
@@ -28,27 +30,50 @@ public class Pawn extends Piece {
         final List<Move> legalMoves = new ArrayList<>();
 
 
-        for (final int currentcandidateOfset : CANDIDATE_MOVE_VECTOR_COORDINATES) {
-            final int candidateDestinationCoordinate = this.piecePosition + (this.getPieceAlliance().getDirection() * currentcandidateOfset;
+        for (final int currentСandidateOfset : CANDIDATE_MOVE_COORDINATE) {
+
+            final int candidateDestinationCoordinate = this.piecePosition + (this.pieceAlliance.getDirection() * currentСandidateOfset);
 
             if (!BoardUtils.isValidateCoordinate(candidateDestinationCoordinate)) {
                 continue;
             }
-            if (currentcandidateOfset == 8 && board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+            if (currentСandidateOfset == 8 && board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+
                 legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
-            } else if (currentcandidateOfset == 16 && this.isFirstMove() &&
+            } else if (currentСandidateOfset == 16 && this.isFirstMove() &&
                     (BoardUtils.SECOND_ROW[this.piecePosition] && this.getPieceAlliance().isBlack()) ||
                     (BoardUtils.SEVENTH_ROW[this.piecePosition] && this.getPieceAlliance().isWhite())) {
                 final int behintCandidateDestinationCoordinate = this.piecePosition + (this.pieceAlliance.getDirection() + 8);
                 if (!board.getTile(behintCandidateDestinationCoordinate).isTileOccupied() &&
                         !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
                     legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
-
-
                 }
 
+            }else if(currentСandidateOfset==7 &&
+                    !((BoardUtils.EIGHTH_COLUMN[this.piecePosition]&& this.pieceAlliance.isWhite()||
+                    (BoardUtils.FIRST_COLUMN[this.piecePosition]&&this.pieceAlliance.isBlack())))){
+                if(board.getTile(candidateDestinationCoordinate).isTileOccupied()){
+                    final  Piece pieceOnCandidate = board.getTile(candidateDestinationCoordinate).getPiece();
+                    if(this.pieceAlliance!=pieceOnCandidate.getPieceAlliance()){
+                      // TODO more to do here
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+
+                    }
+                }
+
+            } else if(currentСandidateOfset==9&&
+                    !((BoardUtils.FIRST_COLUMN[this.piecePosition]&& this.pieceAlliance.isWhite()||
+                      (BoardUtils.EIGHTH_COLUMN[this.piecePosition]&&this.pieceAlliance.isBlack())))){
+                if(board.getTile(candidateDestinationCoordinate).isTileOccupied()){
+                    final  Piece pieceOnCandidate = board.getTile(candidateDestinationCoordinate).getPiece();
+                    if(this.pieceAlliance!=pieceOnCandidate.getPieceAlliance()){
+                        // TODO more to do here
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+
+                    }
+                }
             }
         }
-        return legalMoves;
+        return ImmutableList.copyOf(legalMoves);
     }
 }
